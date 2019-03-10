be4a9835be6d0cbeab35bed3cb87862905d5960e
Michael <michaelrogerswanson@gmail.com>
2019-03-09 21:03:07 -0800

    added details

--- client/src/screens/about/details.js
+++ client/src/screens/about/details.js
@@ -3,21 +3,18 @@ import styled from 'styled-components'
 
 import Header from '../../components/header.js'
 import Team from '../../components/team.js'
-import Details from './details.js'
 
 const Wrapper = styled.section`
   position: relative;
 `
 
-const About = () => {
+const Details = () => {
   return (
     <Wrapper>
       <Header text={'The Team'} />
       <Team />
-
-      <Route path={`${match.path}/:id`} component={Details} />
     </Wrapper>
   )
 }
 
-export default About
+export default Details
