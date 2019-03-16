c856151cb36ca64713cfe031346a6b2108e37542
Huy Nguyen <huy9997@gmail.com>
2019-03-15 22:31:58 -0700

    playing with making the databse connect to aws

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const database = require('./database/queries');
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+database();
+// create a GET route
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
+
