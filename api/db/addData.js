0e3f369b609c09668caace635b7088ada641d9c8
Huy Nguyen <huy9997@gmail.com>
2019-03-23 16:57:14 -0700

    made a skeleton of working db and tested endpoints

--- api/db/addData.js
+++ api/db/addData.js
@@ -1,16 +1,40 @@
 let post = require('./sequalize');
 
-console.log(post);
-let loginData = ()=>{
+let addCoach = ()=>{
   post.sync()
-  .then(() => User.create({
-    username: 'james',
-    password: 'password'
-    
-  }))
-  .then(userTest => {
-    console.log(userTest.toJSON());
-  });
+    .then(() => Coach.create({
+      username: 'james',
+      password: 'password'
+    }))
+    .then(userTest => {
+      console.log(userTest.toJSON());
+    });
 }
 
-module.exports= loginData;
+let addAdmin = ()=>{
+  post.sync()
+    .then(() => Admin.create({
+      username: 'james',
+      password: 'password'
+    }))
+    .then(userTest => {
+      console.log(userTest.toJSON());
+    });
+}
+
+let addWrestler = ()=>{
+  post.sync()
+    .then(() => User.create({
+      name: 'wrestler',
+      password: 'password'
+    }))
+    .then(userTest => {
+      console.log(userTest.toJSON());
+    });
+}
+
+module.exports= {
+  addAdmin,
+  addCoach,
+  addWrestler
+};
--- api/db/index.js
+++ api/db/index.js
@@ -1,17 +1,35 @@
 const Sequelize = require('sequelize');
 const sequelize = require('./sequalize');
-const addData = require('./addData');
+const add = require('./addData');
+const DBtest = require('./DBtest');
 
 let database = () =>{
-
   User = sequelize.define('wrestler',{
+    username: Sequelize.STRING,
+    weight: Sequelize.STRING,
+    age: Sequelize.INTEGER,
+    weightClass: Sequelize.INTEGER
+  });
+
+  Admin = sequelize.define('admin',{
     username: Sequelize.STRING,
     password: Sequelize.STRING,
     age: Sequelize.INTEGER,
     weightClass: Sequelize.INTEGER
   });
 
-  addData();
+  Coach = sequelize.define('coach',{
+    username: Sequelize.STRING,
+    password: Sequelize.STRING,
+    age: Sequelize.INTEGER,
+    weightClass: Sequelize.INTEGER
+  });
+  
+  DBtest();
+  add.addAdmin();
+  add.addCoach();
+  add.addWrestler();
+
 };
 
 module.exports.database = database; 
