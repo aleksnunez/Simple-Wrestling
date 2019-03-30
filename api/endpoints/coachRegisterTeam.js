4285d6820af6f0e39588a2775dc17af9649e0ccc
Huy Nguyen <huy9997@gmail.com>
2019-03-29 22:44:53 -0700

    making changes to the test endpoint

--- api/endpoints/coachRegisterTeam.js
+++ api/endpoints/coachRegisterTeam.js
@@ -4,19 +4,19 @@ const router  = express.Router();
 
 
 router.get('/', function (req, res) {
-
-  res.json('hello world')
+  res.json('sucessfully got data.')
 })
+
+/*
+Name
+Email
+Password
+Organizations
+School / club
+*/
 
 router.post('/hello', function (req,res){
   res.send('hello world');
 })
 
-
-/*
-coaches can register a team
- request
-  - get get data from endpoint to store to database
-  - post names of wrestlers to display
-*/
 module.exports = router;
