84140bbf899f998bb76232b3af1610ba9ceba5bb
MDavid599 <mikeyrocks692@gmail.com>
2019-04-09 17:54:53 -0700

    Sequelize updated in this version, need to check routes and confer with Huy on routes conflicts

--- api/db/index.js
+++ api/db/index.js
@@ -1,12 +1,60 @@
 const Sequelize = require('sequelize');
-const connection = require('./sequalize');
+const awsKeys = require('../config/awsConfig')
+
+
+/*const connection = require('./sequalize');
 const DBtest = require('./DBtest');
 const query = require('./query');
-const add = require('./addData');
+const add = require('./addData');*/
+
 
 let database = () =>{
+  const sequelize = new Sequelize(awsKeys.databaseName, awsKeys.masterUserName, awsKeys.password, {
+    host: 'localhost',
+    dialect: 'postgres',
+    port: 8000,
+  });
 
-  User = connection.define('user',{
+  sequelize
+  .authenticate()
+    .then(() => {
+      console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+      console.error('Unable to connect to the database:', err);
+    });
+
+  const model = Sequelize.Model
+
+  class Wrestler extends model {}
+  Wrestler.init({
+    // attributes
+    userName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    school: {
+      type: Sequelize.STRING
+      // allowNull defaults to true
+    }
+  }, {
+    sequelize,
+    modelName: 'wrestler'
+    // options
+  });
+
+  Wrestler.sync({ force: true }).then(() => {
+    // Now the `users` table in the database corresponds to the model definition
+    return Wrestler.create({
+      userName: 'Mike',
+      school: 'SFSU'
+    });
+  });
+
+  Wrestler.findAll().then(users => {
+    console.log("All users:", JSON.stringify(users, null, 4));
+  });
+  /*User = connection.define('user',{
     username: Sequelize.STRING,
     password: Sequelize.STRING,
     age: Sequelize.INTEGER,
@@ -29,7 +77,8 @@ let database = () =>{
   add.addCoach();
   query.queryFindAll().then((admin)=>{
     console.log(admin);
-  });
+  });*/
+
 }
 
 module.exports.database = database;
--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 const database = require('./db');
 const endpoints = require('./endpoints');
 
-//database.database();
+database.database();
 app.use('/api',endpoints);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
