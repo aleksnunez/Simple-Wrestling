a41006b2d91da687ebc20538196efc0b957f87fa
MDavid599 <mikeyrocks692@gmail.com>
2019-04-19 17:28:02 -0700

    Database abstracted up with pg pool need to figure out pgp

---
+++ api/db/index.js
@@ -0,0 +1,79 @@
+const queries = require "./queries"
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'wrestlingUsername',
+  host: 'wrestlingidentifier.chgoxg8wubk2.us-west-2.rds.amazonaws.com',
+  database: 'WrestlingDatabaseName',
+  password: 'wrestle123',
+  port: 5432,
+})
+
+const addCoach = (request, response) => {
+  pool.query(queries.CREATE_COACH,[coach_name, email, password] (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`User added with ID: ${result.insertId}`)
+  })
+}
+
+const addAdmin = (request, response) => {
+  pool.query(queries.CREATE_ADMIN,[admin_name, email, password] (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`User added with ID: ${result.insertId}`)
+  })
+}
+
+const addWrestler = (request, response) => {
+  pool.query(queries.CREATE_WRESTLER,[user_name, email, password] (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`User added with ID: ${result.insertId}`)
+  })
+}
+
+const deleteCoach = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query(REMOVE_COACH_BY_ID, [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`User deleted with ID: ${id}`)
+  })
+}
+
+const deleteAdmin = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query(REMOVE_ADMIN_BY_ID, [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`User deleted with ID: ${id}`)
+  })
+}
+
+const deleteWrestler = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query(REMOVE_WRESTLER_BY_ID, [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`User deleted with ID: ${id}`)
+  })
+}
+
+module.exports = {
+  addCoach,
+  addAdmin,
+  addWrestler,
+  deleteCoach,
+  deleteAdmin,
+  deleteWrestler
+}
