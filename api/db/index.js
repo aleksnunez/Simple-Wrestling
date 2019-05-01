dc7f58734ae13ab3a864e182d82ee8b48226684a
swabisan <michaelrogerswanson@gmail.com>
2019-04-30 19:04:15 -0700

    removed comments

--- api/db/index.js
+++ api/db/index.js
@@ -50,42 +50,6 @@ let database = (searchTerm) =>{
       school: 'SFSU'
     });
   });
-
-
-  // Model.findAll({
-  //   where: {
-  //     userName: searchTerm
-  //   }
-  // });
-
-  /*Wrestler.findAll().then(users => {
-    console.log("All users:", JSON.stringify(users, userName, school));
-  });*/
-  /*User = connection.define('user',{
-    username: Sequelize.STRING,
-    password: Sequelize.STRING,
-    age: Sequelize.INTEGER,
-    weightClass: Sequelize.INTEGER
-  });
-=======
-
->>>>>>> 46d61f99dd3e3dfab22f79c8497d2bfbba03bd95
-
-let database = () =>{
-
-<<<<<<< HEAD
-  Coach = connection.define('coach',{
-    username: Sequelize.STRING,
-    password: Sequelize.STRING,
-    age: Sequelize.INTEGER,
-    weightClass: Sequelize.INTEGER
-  });
-  add.addCoach();
-  query.queryFindAll().then((admin)=>{
-    console.log(admin);
-  });*/
-
-
 }
 
 module.exports.database = database;
