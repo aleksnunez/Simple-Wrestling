ca5c9aff3a2c7bf7ebb0fde4ead61f4e21f1421f
Huy Nguyen <huy9997@gmail.com>
2019-05-08 17:33:25 -0700

    working on cleaning up sign up

--- api/endpoints/signup.js
+++ api/endpoints/signup.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 const emailValidator = "email-validator";
+const bcrypt = "bcrypt";
 
 function validUser(user) {
   const validEmail = true; //emailValidator.validate(user.email)
@@ -21,50 +22,20 @@ router.post("/addCoach", (req, res) => {
 
   console.log("\n", `received POST: ${JSON.stringify(req.body)}`);
   console.log(`querying database with values: ${JSON.stringify(values)}`);
-  console.log("values", values[0]);
-  // db.searchCoach(values[0])
-  //   .then(query => {
-  //     console.log("query data");
-  //     console.log(`${query}`);
-  //     if (!query) {
-  //       console.log(`this is the result of the query ${query}`);
-  //     }
-  //   })
-  //   .catch(err => {
-  //     console.error(err.stack, "\n");
-  //     res.json(err);
-  //   });
+
   db.addCoach(values)
     .then(query => {
-      // console.log(`sending response: ${JSON.stringify(query)}`, "\n");
-      console.log(query, "success");
-      res.json(query);
+      console.log(query);
+      bcrypt.hash(password, 10, function(err, hash) {
+        // Store hash in your password DB.
+        console.log(hash);
+        res.json(query);
+      });
     })
     .catch(err => {
-      // console.error(err.stack, "\n");
-      console.log("key already exist ");
+      console.error(err.stack, "\n");
       res.json(err);
     });
-});
-
-router.post("/", function(req, res) {
-  const { email, password } = req.body;
-  const values = [email, password];
-  console.log(req.body.email);
-  if (true) {
-    console.log("valid user his email is " + req.body);
-    db.searchCoach(values)
-      .then(query => {
-        console.log(`results of the query sending response: ${query}`, "\n");
-        res.json(query);
-      })
-      .catch(err => {
-        console.log(err.stack);
-        res.json(err);
-      });
-  } else {
-    res.json("sorry please enter in a valid username and password");
-  }
 });
 
 module.exports = router;
--- api/index.js
+++ api/index.js
@@ -18,17 +18,19 @@ app.use("/api", endpoints);
 //error handles
 
 app.use(function(req, res, next) {
-  let err = new Error("Not Found");
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
+// error handler
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: req.app.get("env" === "development" ? err : {})
-  });
-});
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
