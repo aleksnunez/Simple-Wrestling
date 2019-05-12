e37c7ce297a709e751568005c73b731af14cb616
Huy Nguyen <huy9997@gmail.com>
2019-05-12 10:51:39 -0700

    working on cookie login

--- client/src/screens/login/index.js
+++ client/src/screens/login/index.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import request from 'api'
+import React, { useState } from "react";
+import styled from "styled-components";
+import request from "api";
 
-import LoginForm from 'components/forms/loginForm'
+import LoginForm from "components/forms/loginForm";
 
 const Col = styled.section`
   display: flex;
@@ -10,31 +10,31 @@ const Col = styled.section`
   align-items: center;
 
   margin-top: 2em;
-`
+`;
 
 const Login = props => {
-  const [state, setState] = useState({})
+  const [state, setState] = useState({});
 
-  const handleChange = (e) => {
+  const handleChange = e => {
     setState({
       ...state,
       [e.target.name]: e.target.value
-    })
-  }
+    });
+  };
 
-  const onSubmit = (e) => {
-    e.preventDefault()
+  const onSubmit = e => {
+    e.preventDefault();
 
-    request({endpoint: '/api/login/post', body: JSON.stringify(state)})
-    .then(res => alert(JSON.stringify(res)))
-    .catch(err => new Error(err))
-  }
+    request({ endpoint: "/api/login/", body: JSON.stringify(state) })
+      .then(res => alert(JSON.stringify(res)))
+      .catch(err => new Error(err));
+  };
 
   return (
     <Col>
       <LoginForm submit={onSubmit} onchange={handleChange} />
     </Col>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
