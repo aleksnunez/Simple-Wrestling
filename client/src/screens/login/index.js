6f97137f0a8fab8787031c249d5f5eef11a7c5de
Michael Swanson <michaelrogerswanson@gmail.com>
2019-05-12 19:40:23 -0700

    Fix merge conflicts
    I just overrided it with what I have from client-swanson, should be up to date

--- client/src/screens/login/index.js
+++ client/src/screens/login/index.js
@@ -1,14 +1,10 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import request from "api";
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import request from 'api'
 
-<<<<<<< HEAD
-import LoginForm from "components/forms/loginForm";
-=======
 import { required, isEmail } from 'util/formControl/validators'
-import { updateForm, isValid } from 'util/formControl'
+import { updateForm, isValid, formValues, formErrors } from 'util/formControl'
 import LoginForm from 'components/forms/loginForm'
->>>>>>> 5f968c153fa3153a40ad31913ede18e957f9e4c7
 
 const Col = styled.section`
   display: flex;
@@ -16,62 +12,35 @@ const Col = styled.section`
   align-items: center;
 
   margin-top: 2em;
-`;
+`
 
 const Login = props => {
-<<<<<<< HEAD
-  const [state, setState] = useState({});
-
-  const handleChange = e => {
-    setState({
-      ...state,
-      [e.target.name]: e.target.value
-    });
-  };
-=======
   const [formControl, setFormControl] = useState({
     email: {validators: [required, isEmail]},
     password: {validators: [required]}
   })
+  const formData = formValues(formControl)
+  const disabled = !isValid(formControl)
+  const errors = formErrors(formControl)
 
-  const onChange = (e) => {
-    updateForm(e, formControl, setFormControl)
-  }
+  const onChange = (e) => updateForm(e, formControl, setFormControl)
 
-  const isDisabled = () => {
-    return !isValid(formControl)
-  }
->>>>>>> 5f968c153fa3153a40ad31913ede18e957f9e4c7
+  const onSubmit = (e) => {
+    e.preventDefault()
 
-  const onSubmit = e => {
-    e.preventDefault();
-
-<<<<<<< HEAD
-    request({ endpoint: "/api/login/", body: JSON.stringify(state) })
-      .then(res => alert(JSON.stringify(res)))
-      .catch(err => new Error(err));
-  };
-=======
     request({
-      endpoint: '/api/login/post',
-      body: JSON.stringify(formControl)
+      endpoint: '/api/login',
+      body: JSON.stringify(formData)
     })
       .then(res => alert(JSON.stringify(res)))
       .catch(err => new Error(err))
   }
 
-  // looking for a way to generate programmatically
-  const errors = {
-    email: formControl.email.errors,
-    password: formControl.password.errors
-  }
->>>>>>> 5f968c153fa3153a40ad31913ede18e957f9e4c7
-
   return (
     <Col>
-      <LoginForm {...{onChange, isDisabled, onSubmit, errors}} />
+      <LoginForm {...{onChange, disabled, onSubmit, errors}} />
     </Col>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
