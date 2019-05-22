e1d08c1763db3cdf0c8270a46a25041551a3301b
swabisan <michaelrogerswanson@gmail.com>
2019-05-22 12:21:27 -0700

    formatting

--- client/src/containers/navbar/index.js
+++ client/src/containers/navbar/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react'
+import styled from 'styled-components'
 
-import Link from "./link";
-import Button from "./button";
-import Logo from "./logo";
+import Link from './link'
+import Button from './button'
+import Logo from './logo'
 
 const Row = styled.div`
   display: flex;
@@ -16,27 +16,27 @@ const Nav = styled(Row)`
   align-items: center;
   width: 96vw;
   z-index: 100;
-`;
+`
 
 const NavBar = props => {
   return (
     <Nav>
       <Row>
-        <Link to="/">
+        <Link to='/'>
           <Logo />
         </Link>
       </Row>
       <Row>
-        <Link to="/tournaments">Tournaments</Link>
-        <Link to="/about">About</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Sign up</Link>
+        <Link to='/tournaments'>Tournaments</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/login'>Login</Link>
+        <Link to='/signup'>Sign up</Link>
       </Row>
       <Row>
         <Button onClick={props.changeTheme}>Darkmode</Button>
       </Row>
     </Nav>
-  );
-};
+  )
+}
 
-export default NavBar;
+export default NavBar
